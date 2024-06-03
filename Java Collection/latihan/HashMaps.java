d1bf5853e854d4b70b638ec448fceb08d98dee5c
ciello <161020829+ciell0@users.noreply.github.com>
2024-06-03 19:18:47 +0700

    Update HashMaps.java

---
+++ Java Collection/latihan/HashMaps.java
@@ -0,0 +1,14 @@
+package latihan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Ciello", "Belleza");
+
+        System.out.println(map.get("Ciello"));
+    }
+    
+}
